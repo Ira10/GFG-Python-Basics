5ca394bab6d3ca53e920f34fdb89d20677445eff
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-03-13 21:06:46 +0530

    Add files via upload

---
+++ 2. Find Maximum of two numbers in Python - 13th March.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a = int(input("first number "))
+b = int(input("second number "))
+
+if a > b:
+    print(str(a) + " is max")
+else:
+    print(str(b) + " is max")
+
+
+# In[ ]:
+
+
+
+
