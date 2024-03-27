eae7dd408b14c073f10326cd4603eeefaad4b405
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-03-27 23:42:42 +0530

    Add files via upload

---
+++ Educative + book - 27th March.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+7 + 3*(4+5)
+
+
+# In[3]:
+
+
+7 + 3 (4 + 5)
+
+
+# In[6]:
+
+
+prod = 5 * 7
+print ("product:")
+print (prod)
+
+
+# In[7]:
+
+
+prod = 5 * 7
+print ("product:", prod)
+
+
+# In[8]:
+
+
+hello
+
+
+# In[9]:
+
+
+"hi"
+
+
+# In[10]:
+
+
+print("hi")
+
+
+# In[13]:
+
+
+a = "hi8"
+
+print(type(a))
+
+
+# In[15]:
+
+
+a = 8
+
+print(type(a))
+
+
+# In[16]:
+
+
+a = "8"
+
+print(type(a))
+
+
+# In[17]:
+
+
+5 % 10 + 10 < 50 and 29>= 29
+
+
+# In[18]:
+
+
+5 % 10 + 10 < 50 & 29>= 29
+
+
+# # Pay calculator
+# This program should calculate the pay of an employee based on hours worked. The input includes the employee’s total hours worked per week and their hourly pay rate. The employee will be paid a base wage for the first 40 hours worked and time-and-a-half (150% of base pay) for any hours past 40 as overtime pay. Output the regular pay, overtime pay, and total pay for the week on the screen.
+# 
+# If the employee worked 40 hours or less, don’t show any output regarding overtime pay.
+
+# Sample input 1
+# 
+# 30
+# 10
+# Sample output 1
+# 
+# Regular pay: 300
+# Sample input 2
+# 
+# 50
+# 10
+# Sample output 2
+# 
+# Regular pay: 400
+# Overtime pay: 150
+# Total pay: 550
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
